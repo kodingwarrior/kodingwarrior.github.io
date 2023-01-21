1d813233b38953b8f38d304fc0a8faf375a3f0b1
Jaeyeol Lee <rijgndqw012@gmail.com>
2023-01-21 17:52:14 +0900

    (core/wiki) Refactor some codes into SiteBuilder's method

--- plugins/builders/backlink_builder.rb
+++ plugins/builders/backlink_builder.rb
@@ -2,12 +2,7 @@ class Builders::BacklinkBuilder < SiteBuilder
   # See https://github.com/vasturiano/force-graph/blob/master/example/datasets/miserables.json
   def build
     hook :site, :pre_render do |site|
-      resources = site.resources
-      wiki_documents = 
-        site.resources
-          .select { |resource| resource.absolute_url.include?('/wiki/') }
-          .reject { |resource| resource.absolute_url.include?('/templates/') }
-          .reject { |resource| resource.absolute_url.end_with?('/wiki/') }
+      wiki_documents = collect_wiki_resources(site)
 
       nodes = Set.new
       links = []
--- plugins/site_builder.rb
+++ plugins/site_builder.rb
@@ -1,4 +1,10 @@
 class SiteBuilder < Bridgetown::Builder
   # write builders which subclass SiteBuilder in plugins/builders
+  def collect_wiki_resources(site)
+    site.resources
+      .select { |resource| resource.absolute_url.include?('/wiki/') }
+      .reject { |resource| resource.absolute_url.include?('/templates/') }
+      .reject { |resource| resource.absolute_url.end_with?('/wiki/') }
+  end
 end
 
