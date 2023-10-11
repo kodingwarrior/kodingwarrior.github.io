54bc093376b19b6cc62318e5d21996cbc78f35b0
malkoG <rijgndqw012@gmail.com>
2023-10-12 03:17:32 +0900

    (core/wiki) Apply `sunflower seed arrangements` for better placement of the hashtag nodes
    (thanks to @sonohoshi)
    
    * Applied Sunflower Seed Arrangements algorithm
      for rearranging the coordinates of the hashtag nodes.
    * For better visualization, fixed hashtag node's coordinates

--- plugins/builders/backlink_builder.rb
+++ plugins/builders/backlink_builder.rb
@@ -44,7 +44,7 @@ class Builders::BacklinkBuilder < SiteBuilder
       reference_graph = { 
         "nodes": [ 
           *nodes.to_a.map { |name| ({ id: name, group: 1 }) },
-          *node_groups.to_a.map { |name| ({ id: name, group: 2 }) }
+          *rearrange_coordinates(node_groups.to_a.map { |name| ({ id: name, group: 2 }) })
         ],
         "links": links,
       } 
@@ -53,4 +53,42 @@ class Builders::BacklinkBuilder < SiteBuilder
       File.write target_file, JSON.pretty_generate(reference_graph)
     end
   end
+
+  private
+    # Applied Sunflower Seed Arrangements
+    #
+    # See https://demonstrations.wolfram.com/SunflowerSeedArrangements/
+    def rearrange_coordinates(node_groups)
+      node_groups.zip(sunflower(node_groups.size)).map { |f, s| f.merge(s) }
+    end
+
+    def sunflower(n, alpha = 0, geodesic = false)
+      phi = (1.0 + Math.sqrt(5)) / 2.0
+      
+      points = []
+      angle_stride = if geodesic
+        360.0 * phi
+      else
+        2.0 * Math::PI / (phi ** 2)
+      end
+
+      b = (alpha * Math.sqrt(n)) # number of boundary points
+      (1..n).each do |k|
+        r = 100 + radius(k, n, b)
+        theta = k * angle_stride
+        
+        # calculates fixed (x,y) coordinates for hashtag nodes with randomness
+        points << { fx: r * Math.cos(theta), fy: r * Math.sin(theta) }
+      end
+
+      points
+    end
+
+    def radius(k, n, b)
+      if k > n - b
+        1.0
+      else
+        Math.sqrt(k - 0.5) / Math.sqrt(n - (b + 1) / 2.0)
+      end
+    end
 end
