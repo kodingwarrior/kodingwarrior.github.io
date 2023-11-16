d1ba6302d8d67af480a327104547d3e53156cd18
malkoG <rijgndqw012@gmail.com>
2023-11-16 13:32:48 +0900

    (core/wiki) Extract plain text from markdown text inside toc nodes

--- plugins/builders/toc_builder.rb
+++ plugins/builders/toc_builder.rb
@@ -1,3 +1,6 @@
+require 'redcarpet'
+require 'redcarpet/render_strip'
+
 class Builders::TocBuilder < SiteBuilder
   def build
     helper :toc, :toc_template
@@ -6,33 +9,40 @@ class Builders::TocBuilder < SiteBuilder
   def toc_template(input)
     document = Kramdown::Document.new(input)
     toc_nodes = Kramdown::Converter::Toc.convert(document.root)
-    puts toc_nodes.inspect
 
     html = toc_nodes
       .reject { _1.is_a?(Array) && _1.empty? }
       .map { |toc_node| generate_html(toc_node) }
       .join
 
-
     html.html_safe
   end
 
-  def generate_html(toc_node)
-    return "" if toc_node.children.empty?
+  private
+    def generate_html(toc_node)
+      return "" if toc_node.children.empty?
 
-    html = ["<ul>"]
-    toc_node.children.each do |child|
-      html << "<li>"
-      if child.value.options[:raw_text].present?
-        anchor_node = child
-        anchor_text = child.value.options[:raw_text]
-        html << "<a href='##{anchor_node.attr[:id]}'>#{anchor_text}</a>"
+      html = ["<ul>"]
+      toc_node.children.each do |child|
+        html << "<li>"
+        if child.value.options[:raw_text].present?
+          anchor_node = child
+          anchor_text = extract_plain_text_from(child)
+          html << "<a href='##{anchor_node.attr[:id]}'>#{anchor_text}</a>"
+        end
+        html << "</li>"
+        html << generate_html(child) unless child.children.empty?
       end
-      html << "</li>"
-      html << generate_html(child) unless child.children.empty?
-    end
-    html << "</ul>"
+      html << "</ul>"
 
-    html.join
-  end
+      html.join
+    end
+
+    def extract_plain_text_from(toc_node)
+      plain_text = Redcarpet::Markdown.new(Redcarpet::Render::StripDown).render(toc_node.value.options[:raw_text])
+      # extract plain text with given format `plaintext(<http/https url pattern>)`
+      plain_text = plain_text.match(/((?<plaintext>.+))\((?<url>https?:\/\/[^\)]+)\)/)&.[](:plaintext) || plain_text
+
+      plain_text
+    end
 end
